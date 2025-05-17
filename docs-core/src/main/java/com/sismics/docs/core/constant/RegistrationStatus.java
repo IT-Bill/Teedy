9435b85531935fe2c9275b4a1f91435d4eaac108
IT-Bill <704317542@qq.com>
2025-05-17 18:12:35 +0800

    1. Update RegistrationRequest model & dao & dto 2. Add RegistrationStatus constants

---
+++ docs-core/src/main/java/com/sismics/docs/core/constant/RegistrationStatus.java
@@ -0,0 +1,23 @@
+package com.sismics.docs.core.constant;
+
+/**
+ * Registration request status.
+ *
+ * @author Copilot
+ */
+public enum RegistrationStatus {
+    /**
+     * The request is pending approval.
+     */
+    PENDING,
+    
+    /**
+     * The request has been approved.
+     */
+    APPROVED,
+    
+    /**
+     * The request has been rejected.
+     */
+    REJECTED
+}
